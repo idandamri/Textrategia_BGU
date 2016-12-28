8ca46d02e3e86898631cd9ad613b3fd5cb2e7ffc
idandamri <idandamri@gmail.com>
2016-12-28 10:29:24 +0200

    added first instance of DB

---
+++ Instances for DB.sql
@@ -0,0 +1,15 @@
+/*-----------------------------------*/
+insert into users
+values (1,"Damri","Idan","Techni","Beer Sheva",1,"idandamri@gmail.com","123456");
+
+insert into users
+values (2,"Krigel","Shaked","Mekif H","Ashdod",2,"‫shakedkr@post.bgu.ac.il‬‏","123456");
+
+insert into users
+values (3,"Ganim","Hadas","Mevuot","Ashdod",2,"‫HADASGANIM@gmail.com‬‏","123456");
+
+insert into users
+values (4,"Sturm","Arnon","BGU","Beer Sheva",1,"sturm@bgu.ac.i‬‏","123456");
+/*-----------------------------------*/
+
+
