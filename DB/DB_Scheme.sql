90dde013cbc9070ea2dceadbc1df67d797708e90
idandamri <idandamri@gmail.com>
2017-01-03 22:18:45 +0200

    Added the DB scheme ER Diagram

--- DB/DB_Scheme.sql
+++ DB/DB_Scheme.sql
@@ -1,19 +1,19 @@
-CREATE DATABASE textra_db;
-Use textra_db;
+create database textra_db;
+use textra_db;
 
-CREATE TABLE users (
+create table users (
 PersonalID varchar(10) not null,
 LastName varchar(255) not null,
 FirstName varchar(255) not null,
 School varchar(255) not null,
 City varchar(255) not null,
-UserType INT not null,
+UserType int not null,
 Email varchar(255) not null,
 Pass varchar(255) not null,
 primary key (PersonalID)
 );
 
-CREATE TABLE questions
+create table questions
 (
 Q_id bigint unsigned not null,
 Q_qeustion varchar(500) not null,
@@ -30,7 +30,7 @@ Q_disabled boolean not null,
 primary key (Q_id)
 );
 
-CREATE TABLE answers
+create table answers
 (
 A_id bigint unsigned not null,
 Q_id bigint unsigned not null,
@@ -39,7 +39,7 @@ isCorrect boolean not null,
 primary key (A_id, Q_id)
 );
 
-CREATE TABLE tasks
+create table tasks
 (
 T_id bigint unsigned not null,
 T_title varchar(100) not null,
@@ -47,7 +47,7 @@ T_description varchar(500) not null,
 primary key (T_id)
 );
 
-CREATE TABLE tasks_joined_with_questions(
+create table tasks_joined_with_questions(
 T_id bigint unsigned not null,
 Q_id bigint unsigned not null,
 foreign key (T_id) references tasks(T_id),
@@ -55,7 +55,7 @@ foreign key (Q_id) references questions(Q_id),
 primary key (T_id, Q_id)
 );
 
-CREATE TABLE groups
+create table groups
 (
 GroupId bigint unsigned not null,
 GroupName varchar(100) not null,
@@ -66,7 +66,7 @@ primary key (GroupId),
 foreign key (teacherID) references users(PersonalID)
 );
 
-CREATE TABLE students_per_group
+create table students_per_group
 (
 StudentId varchar(10) not null,
 GroupId bigint unsigned not null,
@@ -75,7 +75,7 @@ foreign key (GroupId) references groups(GroupId),
 primary key (StudentId, GroupId)
 );
 
-CREATE TABLE mother_of_all_tables
+create table mother_of_all_tables
 (
 instanceTime timestamp not null,
 studentId varchar(10) not null,
