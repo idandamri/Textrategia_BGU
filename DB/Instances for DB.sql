161c2c7a7eb2820594df5f4ccb77b87b20e187c0
Krigel <shaked.krigel@hpe.com>
2017-03-18 21:11:44 +0200

    change db instances

--- DB/Instances for DB.sql
+++ DB/Instances for DB.sql
@@ -3,13 +3,13 @@ insert into users
 values (1,"Damri","Idan","Techni","Beer Sheva",1,"idandamri@gmail.com","123456");
 
 insert into users
-values (2,"Krigel","Shaked","Mekif H","Ashdod",2,"‫shakedkr@post.bgu.ac.il‬‏","123456");
+values (2,"Krigel","Shaked","Mekif H","Ashdod",2,"shakedkr@post.bgu.ac.il","123456");
 
 insert into users
-values (3,"Ganim","Hadas","Mevuot","Ashdod",2,"hadasganim@gmail.com‬‏","123456");
+values (3,"Ganim","Hadas","Mevuot","Ashdod",2,"hadasganim@gmail.com","123456");
 
 insert into users
-values (4,"Sturm","Arnon","BGU","Beer Sheva",1,"sturm@bgu.ac.il‬‏","123456");
+values (4,"Sturm","Arnon","BGU","Beer Sheva",1,"sturm@bgu.ac.il","123456");
 /*------------------------------------------*/
 
 /*-------------- tasks ---------------------*/
