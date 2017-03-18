38b4c7edaaecb603fdc177d39de73d82670787a3
idandamri <idandamri@gmail.com>
2017-03-18 18:05:50 +0200

    added cors & sinon, as well as tested deletion and insertion

--- NodeJS - Express/server_test.js
+++ NodeJS - Express/server_test.js
@@ -1,215 +1,234 @@
-  var express = require('express');
-  var mysql = require('mysql');
-  var path = require('path');
-  var app = express();
-  var queries = require("./queryForDB.js");
-  var bodyParser = require('body-parser');
-  app.use(express.static(__dirname + '/client'));
-
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  // This responds with "Hello World" on the homepage
+var express = require('express');
+var mysql = require('mysql');
+var cors = require('cors');
+app.use(cors());
+var path = require('path');
+var app = express();
+var queries = require("./queryForDB.js");
+var bodyParser = require('body-parser');
+app.use(express.static(__dirname + '/client'));
 
 
-
-  /*
-  app.get('/how_to request_from_data', function (req, res) {
-     console.log("Got a GET request for the homepage");
-     
-     connection.query ("SELECT * FROM textra_db.answers;" , function(err,row,field){
-  	if(err) console.log(err);
-  	else
-  		res.send(row)
-  	});
-
-  })
-  */
-
-  // app.get('/test',function(req,res){
-  //   var query = queries.getAllAnswersToQuestion("1"); 
-  //   connection.query (query , function(err,ans,field){
-  //     if (err){
-  //       console.log(err);  
-  //     }
-  //     else{
-  //       console.log(ans);
-  //       res.send(ans);
-  // app.get('/test',function(req,res){
-  //   var query = queries.getNumberOfQuestionForTask(1);
-  //   connection.query (query , function(err,ans,field){
-  //     if (err){
-  //       console.log(err);
-  //     }
-  //     else{
-  //       console.log(ans);
-  //       res.send(ans);
-  //
-  //     }
-  //   });
-  // });
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// This responds with "Hello World" on the homepage
 
 
-  app.get('/testForTests',function(req,res){
-      res.send("HELLO TESTER");
-  });
+/*
+ app.get('/how_to request_from_data', function (req, res) {
+ console.log("Got a GET request for the homepage");
 
-  app.get('/',function(req,res){
+ connection.query ("SELECT * FROM textra_db.answers;" , function(err,row,field){
+ if(err) console.log(err);
+ else
+ res.send(row)
+ });
+
+ })
+ */
+
+// app.get('/test',function(req,res){
+//   var query = queries.getAllAnswersToQuestion("1");
+//   connection.query (query , function(err,ans,field){
+//     if (err){
+//       console.log(err);
+//     }
+//     else{
+//       console.log(ans);
+//       res.send(ans);
+// app.get('/test',function(req,res){
+//   var query = queries.getNumberOfQuestionForTask(1);
+//   connection.query (query , function(err,ans,field){
+//     if (err){
+//       console.log(err);
+//     }
+//     else{
+//       console.log(ans);
+//       res.send(ans);
+//
+//     }
+//   });
+// });
+
+
+app.get('/testForTests', function (req, res) {
+    res.send("HELLO TESTER");
+});
+
+app.get('/', function (req, res) {
     res.redirect('/index.html');
-  });
+});
+//
+// app.get('/homepage', function (req, res) {
+//     res.sendFile(__dirname + '/homepage.html');
+// });
+//
 
-  app.get('/homepage',function(req,res){
-    res.sendFile(__dirname + '/homepage.html');
-  });
+app.get('/getListOfTasks', function (req, res) {
+    var user_id = req.body.user_id;
+    var query = queries.gelAllTaskTitleByStudentId(user_id);
+    console.log(query);
+    connection.query(query, function (err, tasks, field) {
+        console.log(JSON.stringify(tasks));
+        if (tasks.length > 0)
+            res.status(200).json(tasks);
+        //send list of tasks
+        else
+            res.status(204).send();
+        /*empty content*/
+    });
+});
 
+/*GOOD
+ getTasks
+ get: user_id
+ return: a list of tasks information*/
 
-   app.get('/getListOfTasks',function(req,res){
-     var user_id = req.body.user_id;
-     var query = queries.gelAllTaskTitleByStudentId(user_id);
-     console.log(query);
-     connection.query(query,function (err,tasks,field) {
-       console.log(JSON.stringify(tasks));
-       if (tasks.length > 0)
-          res.status(200).json(tasks);
-       else
-         res.status(204).send(); /*empty content*/
-     });
-  });
-
-  /*GOOD
-  getTasks
-  get: user_id
-  return: a list of tasks information*/
-  
-  app.get('/getTasks', function (req, res) {
+app.get('/getTasks', function (req, res) {
     console.log("Got a get task request");
     var u_id = req.body.user_id;
     var t_id;
     var merge;
-    var listOfmerge=[];
+    var listOfmerge = [];
     console.log(u_id);
-    var query = queries.gelAllTaskTitleByStudentId('1'); /*hard coded. need to change*/
-    connection.query (query , function(err,tasks,field){
-      if (err){
-        console.log(err);
-        res.send("Err in task req: " + err );
-      }
-      else{
-        t_id = tasks[0]["T_id"];
-        console.log(t_id);
-        query = queries.getNumberOfQuestionForTask(t_id); 
-        connection.query (query , function(err,numberOfQuestion,field){
-          if (err){
-           console.log(err);  
-         }
-         else{
-          var i;
-          for (i=0; i<tasks.length ; i++){
-            console.log("num of question:" + numberOfQuestion);
-            merge ={};
-            Object.assign(merge,tasks[i],numberOfQuestion[i]);
-            console.log ("merge:" +merge );
-            listOfmerge.push(merge);
-            //res.send(merge );
-          }
-          console.log("listofmerge: " + listOfmerge);
-          res.send(listOfmerge);
+    var query = queries.gelAllTaskTitleByStudentId('1');
+    /*hard coded. need to change*/
+    connection.query(query, function (err, tasks, field) {
+        if (err) {
+            console.log(err);
+            res.send("Err in task req: " + err);
         }
-      }); 
-      }
+        else {
+            t_id = tasks[0]["T_id"];
+            console.log(t_id);
+            query = queries.getNumberOfQuestionForTask(t_id);
+            connection.query(query, function (err, numberOfQuestion, field) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    var i;
+                    for (i = 0; i < tasks.length; i++) {
+                        console.log("num of question:" + numberOfQuestion);
+                        merge = {};
+                        Object.assign(merge, tasks[i], numberOfQuestion[i]);
+                        console.log("merge:" + merge);
+                        listOfmerge.push(merge);
+                        //res.send(merge);
+                    }
+                    console.log("listofmerge: " + listOfmerge);
+                    res.send(listOfmerge);
+                }
+            });
+        }
     });
 
-  });
+});
 
-  /* GOOD
-  getQuestion
-  get - t_id , user_id
-  return -A list of jsons. The first one is a question info, second to last - answer info */
-  app.get('/getQuestion', function (req, res) {
+/* GOOD
+ getQuestion
+ get - t_id , user_id
+ return -A list of jsons. The first one is a question info, second to last - answer info */
+app.get('/getQuestion', function (req, res) {
     console.log("Got a question request");
-    var user_id= req.body.user_id;
-    var t_id= req.body.t_id;
+    var user_id = req.body.user_id;
+    var t_id = req.body.t_id;
 
     var isTaskExist_query = queries.getTaskDeatils(t_id);
+    if (isTaskExist_query)
+        var query = queries.getQustionByTaskAndUserID(user_id, t_id);
+    /*hard coded. need to change*/
+    console.log(query);
+    connection.query(query, function (err, listOfQuestion, field) {
+        console.log("listOfQuestion.length:" + listOfQuestion.length);
+        if (err) {
+            console.log(err);
+            res.send("ERR in question request:" + err);
+        }
+        else if (listOfQuestion.length == 0) {
+            res.status(204).send("No more Question for this task");
+            /*empty content*/
+        }
+        else {
+            /*if(listOfQuestion.length>1){
+             for(i=0; i<listOfQuestion.length;i++){
+             res.status(200).json(listOfQuestion[i]);
+             }
+             }*/
+            res.status(200).json(listOfQuestion/*[0]["Q_id"]*/);
+        }
 
-    var query = queries.getQustionByTaskAndUserID(user_id,t_id); /*hard coded. need to change*/
-      console.log(query);
-      connection.query (query , function(err,listOfQuestion,field){
-      console.log("listOfQuestion.length:" + listOfQuestion.length)
-        if (err){
-        console.log(err);
-        res.send("ERR in question request:" + err);
-      }
-      else if (listOfQuestion.length==0){
-          res.status(204).send("No more Question for this task"); /*empty content*/
-      }
-      else {
-          res.status(200).json(listOfQuestion[0]["Q_id"]);
-      }
 
-
-      //   var q_id = listOfQuestion[0]["Q_id"];
-      //   console.log(q_id);
-      //   var query = queries.getAllAnswersToQuestion(q_id);
-      //   connection.query (query , function(err,listOfAnswer,field){
-      //     if (err){
-      //      console.log(err);
-      //    }
-      //    else{
-      //     console.log(listOfQuestion);
-      //     res.send(listOfQuestion.concat(listOfAnswer));
-      //
-      //   }
-      // });
+        //   var q_id = listOfQuestion[0]["Q_id"];
+        //   console.log(q_id);
+        //   var query = queries.getAllAnswersToQuestion(q_id);
+        //   connection.query (query , function(err,listOfAnswer,field){
+        //     if (err){
+        //      console.log(err);
+        //    }
+        //    else{
+        //     console.log(listOfQuestion);
+        //     res.send(listOfQuestion.concat(listOfAnswer));
+        //
+        //   }
+        // });
 
     });
-
-  });
+});
 
 
 /*get: student_id, questio_id,task_id,answer_id
-*/
-  function deleteQuestionFromQueue(s_id, t_id, q_id) {
-      var query2 = queries.DeleteQuestionsFromInstance(s_id, t_id, q_id);
-      console.log('\n' + query2 + '\n');
-      connection.query(query2, function (err, ans, field) {
-          if (err) {
-              return false;
-          }
-          else {
-            return true;
-          }
-      });
-  }
-  app.post('/updateAnswer', function (req, res) {
-      var s_id = req.body.s_id;
-      var t_id = req.body.t_id;
-      var q_id = req.body.q_id;
-      var a_id = req.body.a_id;
-      var query = queries.SubmitStudentsAnswerForQuestion(s_id, t_id, q_id, a_id);
-      //console.log('\n' + query + '\n');
-      connection.query(query, function (err, ans, field) {
-          if (err) {
-              console.log(err);
-              res.status(400).send();
-          }
-          else {
-            /*delete q from queue*/
-              console.log('Ended with inserting!\n');
-              var deleteSucceeded = deleteQuestionFromQueue(s_id, t_id, q_id, res);
-              if (deleteSucceeded)
-                  res.status(200).send();
-              else
-                  res.status(400).send();
-          }
+ */
+app.post('/questionDone', function deleteQuestionFromQueue(req, res, err) {
+    var quest_id = req.body.q_id;
+    var stud_id= req.body.s_id;
+    var task_id = req.body.t_id;
 
-      });
-  });
+
+    var query2 = queries.DeleteQuestionsFromInstance(stud_id, task_id, quest_id);
+    console.log('\n' + query2 + '\n');
+    connection.query(query2, function (err, ans, field) {
+        if (err) {
+            return res.status(204);
+        }
+        else {
+            return res.status(200);
+        }
+    });
+});
+
+app.post('/updateAnswer', function (req, res) {
+    var sId = req.body.stud_id;
+    var tId = req.body.task_id;
+    var qId = req.body.quest_id;
+    var aId = req.body.ans_id;
+    var query = queries.SubmitStudentsAnswerForQuestion(sId, tId, qId, aId);
+    //console.log('\n' + query + '\n');
+    connection.query(query, function (err, ans, field) {
+        if (err) {
+            console.log(err);
+            res.status(400).send();
+        }
+        else {
+            // /*delete q from queue*/
+            // console.log('Ended with inserting!\n');
+            // var deleteSucceeded = deleteQuestionFromQueue(sId, tId, qId, res);
+            // connection.query(deleteSucceeded, function (err, ans, field) {
+            //     if (!err)
+            res.status(200).send();
+            //     else
+            //         res.status(400).send();
+            // });
+        }
+
+    });
+});
 
 
 /*GOOD
-get :  user and password
-return : OK or ERROR
-*/
+ get :  user and password
+ return : OK or ERROR
+ */
 // app.post('/login', function (req, res) {
 //   console.log("Got a login request");
 //   var user_name= req.body.user; /* user_name can be id or email */
@@ -231,65 +250,61 @@ return : OK or ERROR
 // });
 
 
-  /*GOOD
-  get :  user and password
-  return : OK or ERROR
-  */
-  app.post('/login', function (req, res) {
-    var user_name= req.body.user; /* user_name can be id or email */
+/*GOOD
+ get :  user and password
+ return : OK or ERROR
+ */
+app.post('/login', function (req, res) {
+    var user_name = req.body.user;
+    /* user_name can be id or email */
     var password = req.body.password;
-    console.log('Got a login request from: \n\n!!!\n\n'+user_name + "," + password);
-    var query = queries.getDataForUserByIdOrEmail(user_name,password);
+    console.log('Got a login request from: \n\n!!!\n\n' + user_name + "," + password);
+    var query = queries.getDataForUserByIdOrEmail(user_name, password);
     console.log("This is the query: " + query);
-    connection.query (query , function(err,ans,field){
-      if(err)   
-        console.log("err" + err);
-      else{
-        console.log("ans:" + ans);
-        //res.send(row[1]);
-        if(ans.length>0){ /*check if the resault is empty*/
-          res.status(200).send('OK.'); /*change to user id*/
-          console.log('OK');
-        }
+    connection.query(query, function (err, ans, field) {
+        if (err)
+            console.log("err" + err);
         else {
-          res.status(401).send('ERROR');
-          console.log('ERROR \n');
+            console.log("ans:" + ans);
+            //res.send(row[1]);
+            if (ans.length > 0) { /*check if the resault is empty*/
+                res.status(200).send('OK.');
+                /*change to user id*/
+                console.log('OK');
+            }
+            else {
+                res.status(401).send('ERROR');
+                console.log('ERROR \n');
+            }
         }
-      }
     });
-  }); 
+});
 
 
-  // app.get('/',function(req,res){
-  //   res.sendFile(__dirname + '/static' );
-  // });
+// app.get('/',function(req,res){
+//   res.sendFile(__dirname + '/static' );
+// });
 
 
-
-
-
-
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '1q2w3e4r',//'123456' to upload
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1q2w3e4r',//'123456' to upload
     database: 'textra_db'
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     console.error(err);
     // connected! (unless `err` is set)
-  });
+});
 
 
+var server = app.listen(8081, function () {
 
+    var host = server.address().address;
+    var port = server.address().port;
 
-  var server = app.listen(8081, function () {
+    console.log("Example app listening at http://%s:%s", host, port)
+});
 
-   var host = server.address().address;
-   var port = server.address().port;
-
-   console.log("Example app listening at http://%s:%s", host, port)
-  });
-
-module.exports =app;
+module.exports = app;
--- NodeJS - Express/test/tools-spec.js
+++ NodeJS - Express/test/tools-spec.js
@@ -5,14 +5,13 @@ var rewire = require('rewire');
 var app = rewire('../server_test');
 //var sinon = require("sinon");
 
-describe("Testing Textrategia API",function(){
+describe("Testing Textrategia API", function () {
 
-
-     it("Loads home page", function (done) {
-         request(app).get("/").expect(200).end(done);
+    it("Loads home page", function (done) {
+        request(app).get("/").expect(200).end(done);
     });
 
-    describe("Testing Login",function() {
+    describe("Testing Login", function () {
         it('Test login Request - correct deatils', function (done) {
             request(app).post("/login").send({"user": "shakedkr@post.bgu.ac.il", "password": "123456"})
                 .expect(200)
@@ -25,7 +24,7 @@ describe("Testing Textrategia API",function(){
         });
     });
 
-    describe("Testing get Tasks",function() {
+    describe("Testing get Tasks", function () {
         it('Test get tasks', function (done) {
             request(app).get("/getListOfTasks").send({"user_id": '1'})
                 .end(function (err, res) {
@@ -43,23 +42,26 @@ describe("Testing Textrategia API",function(){
                 .end(function (err, res) {
                     if (err)
                         throw err;
-                    //console.log("BODY: " + JSON.stringify(res.body));
-                    res.status.should.be.equal(204); /*no data*/
+                    res.status.should.be.equal(204);
+                    /*no data*/
                     done();
                 });
         });
     });
 
-    describe("Testing Get question",function() {
+    describe("Testing Get question", function () {
         it('Get Question For a Task', function (done) {
             request(app).get("/getQuestion").send({"user_id": '1', "t_id": "1"})
                 .end(function (err, res) {
                     if (err)
                         throw err;
                     res.status.should.be.equal(200);
-                    console.log("BODY: " + JSON.stringify(res.body));
+                    questiions = res.body;
+                    for (i = 0; i < questiions.length; i++) {
+                        strQ = JSON.stringify(questiions[i]);
+                        console.log("BODY: " + strQ);
+                    }
                     done();
-                    //res.body.should.be.equal('[{"T_id":1,"T_title":"מטלת ניסוי","T_description":"מטלת ניסוי לבסיס הנתונים"}]')
                 });
         });
 
@@ -69,33 +71,41 @@ describe("Testing Textrategia API",function(){
                     if (err)
                         throw err;
                     res.status.should.be.equal(204);
-                    console.log("BODY: " + JSON.stringify(res.body));
                     done();
-                    //res.body.should.be.equal('[{"T_id":1,"T_title":"מטלת ניסוי","T_description":"מטלת ניסוי לבסיס הנתונים"}]')
+                });
+        });
+    });
+
+    describe("Testing update answer", function () {
+        this.timeout(7000);
+        it('Testing update answer', function (done) {
+            request(app).post("/updateAnswer").send({"ans_id": "1", "quest_id": "1", "stud_id": "2", "task_id": "1"})
+                .end(function (err, res) {
+                    if (err) {
+                        console.log("ERR: " + err)
+                        throw err;
+                    }
+                    else {
+                        res.status.should.be.equal(200);
+                    }
+                    done();
                 });
         });
 
-
+        it('Testing removing quest after update', function (done) {
+            request(app).post("/questionDone")
+                .send({"s_id": "2", "t_id": "1", "q_id": "1"})
+                .end(function (err, res) {
+                    if (err) {
+                        console.log("ERR: " + err)
+                        throw err;
+                        done();
+                    }
+                    else {
+                        res.status.should.be.equal(200);
+                        done();
+                    }
+                });
+        });
     });
-
-    describe ("Testing update answer", function () {
-        // it('Testing success update', function (done) {
-        //     request(app).post("/updateAnswer").send({"a_id ": "1" , "q_id" : "1" , "t_id " : "1" , "s_id" : "2"})
-        //         .end(function (err,res) {
-        //             if(err){
-        //                 console.log("ERR: " + err)
-        //                 throw err;
-        //             }
-        //
-        //             res.status.should.be.equal(200);
-        //             done();
-        //
-        //         });
-        // });
-
-    });
-
-
-
-
 });
