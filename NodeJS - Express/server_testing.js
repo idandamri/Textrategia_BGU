46155157811640e89598c6688b0f316f1ac2fbe6
hadasbgu <hadasganim@gmail.com>
2017-06-06 16:58:18 +0300

    tabs navbar and padding

--- NodeJS - Express/server_testing.js
+++ NodeJS - Express/server_testing.js
@@ -1496,7 +1496,7 @@ app.post('/sendTaskToStudents', function (req, res) {
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1q2w3e4r',//'123456' to upload*/
+    password: '123456',//'1q2w3e4r' to upload*/
     database: 'textra_db',
     multipleStatements: true
 });
@@ -1508,16 +1508,16 @@ connection.connect(function (err) {
 });
 
 
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port)
-});
+// var server = app.listen(8081, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log("Example app listening at http://%s:%s", host, port)
+// });
 
 // TODO - Hadas you need this/TESTS!!!
-// app.listen(8081, "127.0.0.1", function () {
-//     console.log("App is running ");
-// });
+app.listen(8081, "127.0.0.1", function () {
+    console.log("App is running ");
+});
 
 setInterval(function () {
     connection.query('SELECT 1');
