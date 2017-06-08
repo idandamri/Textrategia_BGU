e0b926b1eb3faa784a762e63188517b9cf280c16
idandamri <idandamri@gmail.com>
2017-06-08 15:09:25 +0300

    working on upload... need shakeds help..

--- NodeJS - Express/server_testing.js
+++ NodeJS - Express/server_testing.js
@@ -1503,29 +1503,30 @@ app.post('/getAllSkills', function (req, res) {
 });
 
 
-var storage =   multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, './uploads');
-    },
-    filename: function (req, file, callback) {
-        callback(null, file.fieldname + '-' + Date.now());
-    }
-});
-
-var upload = multer({ storage : storage}).single('userPhoto');
-
-app.get('/',function(req,res){
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.post('/api/photo',function(req,res){
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-});
+// var storage =   multer.diskStorage({
+//     destination: function (req, file, callback) {
+//         callback(null, './uploads');
+//     },
+//     filename: function (req, file, callback) {
+//         callback(null, var afterDot = file.originalname.substr(file.originalname.indexOf('.'));
+//         );
+//     }
+// });
+//
+// var upload = multer({ storage : storage}).single('userPhoto');
+//
+// app.get('/',function(req,res){
+//     res.sendFile(__dirname + "/index.html");
+// });
+//
+// app.post('/api/photo',function(req,res){
+//     upload(req,res,function(err) {
+//         if(err) {
+//             res.status(400).send(req);
+//         }
+//         res.status(200).send(req);
+//     });
+// });
 
 
 app.post('/sendTaskToStudents', function (req, res) {
