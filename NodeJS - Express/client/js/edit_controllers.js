3b071d182235239fe6fe09d664a62dd14432dad1
hadasbgu <hadasganim@gmail.com>
2017-06-12 19:38:06 +0300

    more of managment

--- NodeJS - Express/client/js/edit_controllers.js
+++ NodeJS - Express/client/js/edit_controllers.js
@@ -167,6 +167,12 @@ textrategiaApp.controller("QuestionManagmentController",function($scope,$locatio
         $scope.flagEditQuestionMode = false;
     }
 
+    $scope.backToEditQuestionView = function(){
+    $scope.searchQuestionByProfiling = false;
+    $scope.flagEditQuestionMode = true;
+}
+
+
 
     $scope.getQuestionToEditFromStock = function(){
         $scope.searchQuestionByProfiling = false;
@@ -176,34 +182,37 @@ textrategiaApp.controller("QuestionManagmentController",function($scope,$locatio
        var jasonIndex = get_jason_index($scope.myQuestionsStock ,  $scope.id_question_to_edit);
  
 
-    $scope.jasonOfQuestionToEdit = $scope.myQuestionsStock[jasonIndex];
-    //   $scope.jasonOfQuestionToEdit = $scope.myQuestionsStock[1];
-    
-    $scope.question = {
-        question_title:     $scope.myQuestionsStock[jasonIndex].Q_qeustion,
-        quest_correct_fb:   $scope.myQuestionsStock[jasonIndex].Q_correctFB,
-        quest_incorrect_fb: $scope.myQuestionsStock[jasonIndex].Q_notCorrectFB,
-        media_type:         $scope.myQuestionsStock[jasonIndex].Q_mediaType,
-        question_media:     $scope.myQuestionsStock[jasonIndex].Q_media,
-        selected_skill:     $scope.myQuestionsStock[jasonIndex].Q_skill,
-        difficulty:         $scope.myQuestionsStock[jasonIndex].Q_difficulty
+        $scope.jasonOfQuestionToEdit = $scope.myQuestionsStock[jasonIndex];
+        //   $scope.jasonOfQuestionToEdit = $scope.myQuestionsStock[1];
+        
+        $scope.question = {
+            question_title:     $scope.myQuestionsStock[jasonIndex].Q_qeustion,
+            quest_correct_fb:   $scope.myQuestionsStock[jasonIndex].Q_correctFB,
+            quest_incorrect_fb: $scope.myQuestionsStock[jasonIndex].Q_notCorrectFB,
+            media_type:         $scope.myQuestionsStock[jasonIndex].Q_mediaType,
+            question_media:     $scope.myQuestionsStock[jasonIndex].Q_media,
+            selected_skill:     $scope.myQuestionsStock[jasonIndex].Q_skill,
+            difficulty:         $scope.myQuestionsStock[jasonIndex].Q_difficulty
+        };
 
-    };
+        alert("does jason OF question works? " + jasonOfQuestionToEdit.Q_qeustion);
     
-
     }
 
 
-        // $scope.question.question_title = $scope.myQuestionsStock[jasonIndex].Q_qeustion;
-        // $scope.question.quest_correct_fb = $scope.myQuestionsStock[jasonIndex].Q_correctFB;
-        // $scope.question.quest_incorrect_fb = $scope.myQuestionsStock[jasonIndex].Q_notCorrectFB;
-        // $scope.question.media_type = $scope.myQuestionsStock[jasonIndex].Q_mediaType;
-        // $scope.question.question_media = $scope.myQuestionsStock[jasonIndex].Q_media;
-        // $scope.question.selected_skill = $scope.myQuestionsStock[jasonIndex].Q_skill;
-        // $scope.question.difficulty = $scope.myQuestionsStock[jasonIndex].Q_difficulty;
-       
+// ~~~~~~~~~~~~~~~~~~~~~~~~~ TO DO TO DO TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        // ####################################################
+        // GET POSSIBLE ANSWER HERE!!!!!
+        // ####################################################
 
 
+
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~ /TO DO TO DO TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
@@ -247,44 +256,27 @@ textrategiaApp.controller("QuestionManagmentController",function($scope,$locatio
 
 
     $scope.sendNewQuestion = function (){
-        // question_title is argument 1
-        var question_title = $scope.question.question_title;
 
-        // 3 - 4
+        var question_title = $scope.question.question_title;
         var media_type =  $scope.question.media_type;
         var quest_skill = $scope.question.selected_skill;
         var quest_difficulty = $scope.question.difficulty;
-
-        // arguments 4 - 8
         var question_media = $scope.question.question_media;
         var quest_correct_fb = $scope.question.quest_correct_fb;
         var quest_incorrect_fb = $scope.question.quest_incorrect_fb;
-
-
-
-        // the information:
-        // quest_proffesion /// quest_is_approved //// who_created /// quest_disabled
-        // is not user inserted
 
         // get possible answers infomation!
         var possible_ans_1 = $scope.question.possible_ans_1;
         var possible_ans_2 = $scope.question.possible_ans_2;
         var possible_ans_3 = $scope.question.possible_ans_3;
         var possible_ans_4 = $scope.question.possible_ans_4;
+        
 
-        // (CORRECT ANS) opt3.value is argument
-        var opt3;
-        var sel3 = document.getElementById("correct_ans");
-        for (i = 0 ; i < sel3.options.length ; i++){
-            opt3 = sel3.options[i];
-            if (opt3.selected == true){
-                // alert(opt3.value);              // 0 is ans1 , 1 is ans2 ...
-                break;
-            }
-        }
+        var correct_ans = $scope.question.correct_ans;
 
-        alert("title: " + question_title + " media_type: " + media_type + " quest_difficulty: " + quest_difficulty + " question_media :" + question_media);
-        alert("y: " + quest_correct_fb + " n: " + quest_incorrect_fb + " skill: " + quest_skill );
+       
+        //alert("title: " + question_title + " media_type: " + media_type + " quest_difficulty: " + quest_difficulty + " question_media :" + question_media);
+        //  alert("y: " + quest_correct_fb + " n: " + quest_incorrect_fb + " skill: " + quest_skill );
        // alert("1: " + possible_ans_1 + " 2: " + possible_ans_2 + " 3: " +  possible_ans_3 + " 4: " + possible_ans_4)
 
         // ####################################################
@@ -302,41 +294,41 @@ textrategiaApp.controller("QuestionManagmentController",function($scope,$locatio
         else
             is_approved=0;
 
-        // var req = {
-        //     method: 'POST',
-        //     cache: false,
-        //     url: _url +'/addQuestion',
-        //     headers: {
-        //         'Content-Type': 'application/x-www-form-urlencoded'
-        //     },
-        //     data: 'question_title='+ question_title
-        //     +'&is_multiple_ans='+ '0'
-        //     + '&question_media_type=' + media_type.value
-        //     + '&question_media=' + question_media
-        //     + '&quest_correct_fb=' + quest_correct_fb
-        //     +'&quest_incorrect_fb=' + quest_incorrect_fb
-        //     +'&quest_skill=' +  quest_skill
-        //     + '&quest_difficulty=' +   quest_difficulty.value
-        //     + '&quest_proffesion=' + 'הבעה'
-        //     + '&quest_is_approved=' + is_approved
-        //     + '&quest_disabled=' + '1'
-        //     + '&who_created=' + getUserID()
-        //     + '&answer1=' + possible_ans_1
-        //     + '&answer2=' + possible_ans_2
-        //     + '&answer3=' + possible_ans_3
-        //     + '&answer4=' + possible_ans_4
-        //     + '&correctAnswerIndex=' + opt3.value
-        // };
+        var req = {
+            method: 'POST',
+            cache: false,
+            url: _url +'/editQuestion',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: 'question_title='+ question_title
+            +'&is_multiple_ans='+ '0'
+            + '&question_media_type=' + media_type
+            + '&question_media=' + question_media
+            + '&quest_correct_fb=' + quest_correct_fb
+            +'&quest_incorrect_fb=' + quest_incorrect_fb
+            +'&quest_skill=' +  quest_skill
+            + '&quest_difficulty=' +   quest_difficulty
+            + '&quest_proffesion=' + 'הבעה'
+            + '&quest_is_approved=' + is_approved
+            + '&quest_disabled=' + '1'
+            + '&who_created=' + getUserID()
+            // + '&answer1=' + possible_ans_1
+            // + '&answer2=' + possible_ans_2
+            // + '&answer3=' + possible_ans_3
+            // + '&answer4=' + possible_ans_4
+            // + '&correctAnswerIndex=' + correct_ans
+        };
 
-        // // alert(JSON.stringify(req));
+        // alert(JSON.stringify(req));
 
-        // $http(req)
-        //     .success(function(data,status,headers,config){
-        //         $scope.serverFeedback = "השאלה נשלחה בהצלחה!"
-        //         $scope.doneRegisterQuestion = true;
-        //     }).error(function(data,status,headers,config){
-        //     $scope.serverFeedback = "שגיאה בהכנסת שאלה";
-        // });
+        $http(req)
+            .success(function(data,status,headers,config){
+                $scope.serverFeedback = "השאלה נערכה בהצלחה!"
+                $scope.doneRegisterQuestion = true;
+            }).error(function(data,status,headers,config){
+            $scope.serverFeedback = "שגיאה בהכנסת שאלה";
+        });
 
 
 
