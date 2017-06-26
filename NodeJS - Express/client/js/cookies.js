b3351252a189477d316a5401900304a28f3ea0ff
hadasbgu <hadasganim@gmail.com>
2017-06-26 23:08:11 +0300

    hadass2

---
+++ NodeJS - Express/client/js/cookies.js
@@ -0,0 +1,72 @@
+var userName ="";
+
+function setUserName(_userName){
+	userName = _userName;
+};
+
+function getUserName(){
+	return userName ;
+};
+
+var userID ="";
+
+function setUserID(_userID){
+	userID = _userID;
+};
+
+function getUserID(){
+	return userID ;
+};
+
+var taskID ="";
+
+function setTaskID(_taskID){
+	taskID = _taskID;
+};
+
+function getTaskID(){
+	return taskID ;
+};
+
+var taskName ="";
+
+function setTaskName(_taskName){
+    taskName= _taskName;
+};
+
+function getTaskName(){
+    return taskName ;
+};
+
+var groupCode ="";
+
+function setGroupCode(_groupCode){
+    groupCode= _groupCode;
+};
+
+function getGroupCode(){
+    return this.groupCode;
+};
+
+// var = userType = ""; //0- student ; 1- teacher ; 2 - superUser
+
+// function setUserType(_userType){
+// 	userType = _userType;
+// };
+
+// function getUserType(){
+//     return this.userType;
+// }
+
+var userType ="";
+
+function setUserType(_userType){
+    userType= _userType;
+};
+
+function getUserType(){
+    return this.userType;
+};
+
+
+
