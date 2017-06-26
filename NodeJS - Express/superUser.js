11bbc7fb8000afe53937278a62268fe4ed76fb3b
hadasbgu <hadasganim@gmail.com>
2017-06-26 22:54:05 +0300

    idan q_disabled

--- NodeJS - Express/superUser.js
+++ NodeJS - Express/superUser.js
@@ -294,12 +294,8 @@ router.post('/getReported', function (req, res) {
     try {
         var query = "select * from textra_db.questions where Q_reported_Offensive>=1" +
             " or Q_reported_Question>=1" +
-<<<<<<< HEAD
             " or Q_reported_Answer>=1" +
-            " and Q_disabled = 0 and Q_approved = 1;";
-=======
-            " or Q_reported_Answer>=1;";
->>>>>>> cddece6e4a9b973ec476cb2b3afde57f61d53346
+            " and ( Q_disabled = 0 and Q_approved = 1);";
         console.log('\n' + query + '\n');
         connection.query(query, function (err, questions) {
             if (err) {
