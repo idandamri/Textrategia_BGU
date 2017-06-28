72ad97a9a234f5f5acd9a13d407353a75a366881
hadasbgu <hadasganim@gmail.com>
2017-06-28 13:08:23 +0300

    buttons in createQ

--- NodeJS - Express/server.js
+++ NodeJS - Express/server.js
@@ -359,15 +359,15 @@ connection.connect(function (err) {
 // });
 
 
-// TODO - Hadas you need this/TESTS!!!
-app.listen(8081, "10.0.0.4", function () {
-    console.log("App is running ");
-});
-
-// TODO - Hadas you need this/TESTS!!!
-// app.listen(8081, "127.0.0.1", function () {
+// // TODO - Hadas you need this/TESTS!!!
+// app.listen(8081, "10.0.0.4", function () {
 //     console.log("App is running ");
 // });
+
+// TODO - Hadas you need this/TESTS!!!
+app.listen(8081, "127.0.0.1", function () {
+    console.log("App is running ");
+});
 
 setInterval(function () {
     connection.query('SELECT 6');
